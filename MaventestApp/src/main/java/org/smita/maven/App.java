b4c5d712d69068cc4e29bc3a7dc11ef485d8a4be
sm0017 <smita.sukhadeve@maine.edu>
2015-07-08 21:58:29 -0400

    trial

---
+++ MaventestApp/src/main/java/org/smita/maven/App.java
@@ -0,0 +1,17 @@
+package org.smita.maven;
+import org.slf4j.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      //  System.out.println( "Hello World!" );
+
+	Logger logger = LoggerFactory.getLogger(App.class);
+	logger.info("Hello World");
+    }
+}
